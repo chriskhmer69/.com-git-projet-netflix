d23cac8bdfc0648427476c9ab1f739e60e38093d
Glitch (glitch-hello-website) <none>
2023-03-08 14:00:51 +0000

    👬🌜 Checkpoint ./style.css:50189298/2 ./index.html:50189298/2962 ./script.js:50189298/286

---
+++ script.js
@@ -0,0 +1,10 @@
+   <script>
+      const nav = document.getElementById("nav");
+      window.addEventListener("scroll", () => {
+        if (window.scrollY >= 100) {
+          nav.classList.add("nav__black");
+        } else {
+          nav.classList.remove("nav__black");
+        }
+      });
+    </script>
